9674e435a50c6c4f8d5b4360dc6771a7007fd3e1
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-12-20 19:42:45 +0530

    ui completed

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -1,5 +1,27 @@
+import "./preview.css";
+
 let Preview = () => {
-	return <>Pre</>;
+	return (
+		<>
+			<div className="preview-container">
+				<div className="preview-img-container">
+					<img src="https://i.shgcdn.com/d28da852-3c05-408e-bde3-4aeb881e1a08/-/format/auto/-/preview/3000x3000/-/quality/lighter/"></img>
+				</div>
+				<div className="preview-listing">
+					<h2>Title</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+						voluptatibus ad fugit. Maiores vero magni quisquam architecto
+						doloribus, consequatur nobis ex, tenetur culpa perspiciatis
+						temporibus enim dolor error ad! Iusto facere inventore, a, aliquid
+						non velit excepturi harum, perspiciatis dolorem ipsa corporis
+						consequuntur tempora itaque deleniti eos id doloremque magnam!
+					</p>
+					<button>Add to Cart</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Preview;
